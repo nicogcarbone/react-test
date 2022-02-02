8e5e5f95632ad0d85911cc01a93d4ac87c90fb43
nicolas carbone <nicogabriel.carbone@gmail.com>
2022-02-01 22:13:12 -0300

    Navbar react

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+      <nav>
+      <ul>
+      <li><button>HOME</button></li>
+      <li>ABOUT US</li>
+      <li>CONTACT</li>
+
+      
+      <li><button>LOG IN</button></li>
+      </ul>
+      
+  </nav>
+  
+    );
+}
+export default Navbar
